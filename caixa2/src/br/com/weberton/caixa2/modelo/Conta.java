02d682ddc7ce27e889983e51605977ece129c9e4
WebertonSilva <wfs_be@yahoo.com.br>
2016-11-27 17:53:22 -0200

    sobreescrevendo toString

--- caixa2/src/br/com/weberton/caixa2/modelo/Conta.java
+++ caixa2/src/br/com/weberton/caixa2/modelo/Conta.java
@@ -94,6 +94,7 @@ public abstract class Conta implements Tributavel{
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + agencia;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		result = prime * result + numero;
 		return result;
 	}
@@ -109,14 +110,16 @@ public abstract class Conta implements Tributavel{
 		Conta other = (Conta) obj;
 		if (agencia != other.agencia)
 			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
 		if (numero != other.numero)
 			return false;
 		return true;
 	}
 
-
-
-	
 	
 	
 	
